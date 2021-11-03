dbd8f23bc6eea477960976c691812e77fb2880aa
Hiro <hiroshi.takeuchi111@gmail.com>
2021-11-03 22:53:25 +0000

    fixed get route for ID

--- src/routers/apiRouter.ts
+++ src/routers/apiRouter.ts
@@ -4,7 +4,7 @@ import { ApiController } from "../controllers/apiController";
 export const getApiRouter = (apiController: ApiController) => {
   const apiRouter = Router();
 
-  apiRouter.get("/", apiController.getPosition);
+  apiRouter.get("/:id", apiController.getPosition);
   apiRouter.post("/", apiController.postPosition);
 
   return apiRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { getApiController } from "./controllers/apiController";
+import { getApiRouter } from "./routers/apiRouter";
 
 import { config } from "./utils/config";
 
@@ -7,6 +9,14 @@ const app = express();
 // route for status check
 app.get("/", (req, res) => res.send({ status: "OK" }));
 
+// controllers
+const apiController = getApiController(config);
+// routers
+const apiRouter = getApiRouter(apiController);
+// add route
+app.use("/api", apiRouter);
+
+// start server
 app.listen(config.port, () =>
   console.log(`✨✨ Listening on port ${config.port} ✨✨`)
 );
